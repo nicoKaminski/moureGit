5cd94ad2c280e9594d129bec3c37acf541f447c0
nicoKaminski <murek38@gmail.com>
2025-04-06 14:25:23 -0300

    agrege mensajes desde login

--- hellogit2.py
+++ hellogit2.py
@@ -1,2 +1,3 @@
 print("haciendo otro archivo para seguir aprendiendo")
 print("agrege algo en el main despues de hacer la rama login")
+print ("Mensaje desde login en hello 2")
--- hellogit3.py
+++ hellogit3.py
@@ -1,2 +1,3 @@
 print("git 3")
 print("veremos que cosas se aprende")
+print("Mensaje desde login en hello 3")
